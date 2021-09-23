feb6771feca12a665765a6821f3f9a62870ec7c7
NWBY <sam.newby19@gmail.com>
2021-09-23 23:54:36 +0100

    Create stubs complete

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # go-stubs
 
-Easily create `.go` files from stub files. 
+Easily create `.go` files from stub files in your projects.
+
+### Usage
+
+```bash
+go get github.com/nwby/go-stubs
+```
+
+Create a stub file:
+```go
+package stubs
+
+type {{.Model}} struct {
+	
+}
+```
+
+Define a `StubDetails` struct and then that stub using `CreateStub`:
+```
+stub := StubDetails{
+    Name:        "./stubs/model.go.stub",
+    FileName:    "model.go",
+    Destination: "./",
+    Values: map[string]string{
+        "Model": "User",
+    },
+}
+
+stub.CreateStub()
+```
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type StubDetails struct {
+	Name, FileName, Destination string
+	Values                      map[string]string
+}
+
+func (s *StubDetails) CreateStub() {
+	contentsBuff, err := os.ReadFile(s.Name)
+	if err != nil {
+		log.Fatalf("Unable to read file: %s", s.Name)
+	}
+
+	f, err := os.OpenFile(s.Destination+s.FileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		log.Fatalf("Unable to open file: %s", s.FileName)
+	}
+	defer f.Close()
+
+	template, err := template.New(s.FileName).Parse(string(contentsBuff))
+	if err != nil {
+		log.Fatalf("Unable to parse template: %s", s.Name)
+	}
+	template.Execute(f, s.Values)
+}
+
+func main() {
+	stub := StubDetails{
+		Name:        "./stubs/model.go.stub",
+		FileName:    "model.go",
+		Destination: "./",
+		Values: map[string]string{
+			"Model": "User",
+		},
+	}
+
+	stub.CreateStub()
+}
